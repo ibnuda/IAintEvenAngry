3acfd815096fbc7ee3b44fc039c00f3ffe2395ca
Ibnu Daru Aji <ibnu.d.a@student.uns.ac.id>
2016-01-16 23:57:46 +0700

    New Project?

---
+++ PakaiForm/MainActivity.fs
@@ -0,0 +1,24 @@
+﻿namespace PakaiForm
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+open Android.Widget
+open Xamarin.Forms
+
+[<Activity (Label = "PakaiForm", MainLauncher = true)>]
+type MainActivity () =
+    inherit Activity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        // Set our view from the "main" layout resource
+
